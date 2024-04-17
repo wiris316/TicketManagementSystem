01902966c0cdfe88254c3351dccbf7823fd26f3b
Iris <wiris316@gmail.com>
2024-04-17 19:35:29 -0400

    feat: update ticket service

--- TicketManagementSystem/src/services/api.jsx
+++ TicketManagementSystem/src/services/api.jsx
@@ -1,5 +1,4 @@
-import { addDoc, collection, getDocs, getFirestore } from 'firebase/firestore';
-
+import { addDoc, collection, doc, getDoc, getDocs, getFirestore, onSnapshot, updateDoc } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
@@ -7,10 +6,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore();
+const ticketCollection = collection(firestore, 'ticket');
 
 export async function fetchTickets() {
   try {
-    const ticketCollection = collection(firestore, 'ticket');
     const ticketSnapshot = await getDocs(ticketCollection);
     const ticketData= ticketSnapshot.docs.map(doc => ({ id:doc.id, ...doc.data()}));
     console.log('Successfully fetched ticket data.')
@@ -22,7 +21,6 @@ export async function fetchTickets() {
 
 export async function submitTicket(description, email, name, subject) {
   try {
-    const ticketCollection = collection(firestore, 'ticket');
     await addDoc(ticketCollection, {
       createdAt: new Date(),
       description: description,
@@ -34,5 +32,23 @@ export async function submitTicket(description, email, name, subject) {
     console.log('Successfully submitted ticket.')
   } catch (error) {
     console.error('Error in submitting ticket:', error)
+  }
+}
+
+export async function updateTicket(ticketid, status) {
+  try {
+    const ticketRef = doc(firestore, 'ticket', ticketid)
+    await updateDoc(ticketRef, {
+      status: status,
+      }, { merge: true })
+      .then(() => {
+        console.log('Document successfully updated!');
+      })
+      .catch((error) => {
+          console.error('Error updating document: ', error);
+      });
+
+  } catch (error) {
+    console.error('Error in updating ticket', error);
   }
 }
