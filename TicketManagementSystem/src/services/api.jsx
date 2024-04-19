c5f9d87a003fb2d08ff0603ba9fcc038bc849648
Iris <wiris316@gmail.com>
2024-04-19 13:26:16 -0400

    feat: login functionality

--- TicketManagementSystem/src/services/api.jsx
+++ TicketManagementSystem/src/services/api.jsx
@@ -1,17 +1,19 @@
-import { addDoc, collection, getDocs, getFirestore } from 'firebase/firestore';
-
+import { addDoc, collection, doc, getDoc, getDocs, getFirestore, limit, query, orderBy } from 'firebase/firestore';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore();
+const firestore = getFirestore(app);
+const auth = getAuth();
+const ticketCollection = collection(firestore, 'ticket');
+const sortedQuery = query(ticketCollection, orderBy('createdAt', 'desc'), limit(50));
 
 export async function fetchTickets() {
   try {
-    const ticketCollection = collection(firestore, 'ticket');
-    const ticketSnapshot = await getDocs(ticketCollection);
+    const ticketSnapshot = await getDocs(sortedQuery);
     const ticketData= ticketSnapshot.docs.map(doc => ({ id:doc.id, ...doc.data()}));
     console.log('Successfully fetched ticket data.')
     return ticketData;
@@ -22,7 +24,6 @@ export async function fetchTickets() {
 
 export async function submitTicket(description, email, name, subject) {
   try {
-    const ticketCollection = collection(firestore, 'ticket');
     await addDoc(ticketCollection, {
       createdAt: new Date(),
       description: description,
@@ -34,5 +35,29 @@ export async function submitTicket(description, email, name, subject) {
     console.log('Successfully submitted ticket.')
   } catch (error) {
     console.error('Error in submitting ticket:', error)
+  }
+}
+
+export async function signIn(email, password) {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error('Error signing in', error);
+  }
+}
+
+export async function getUser(email) {
+  try {
+    const docRef = doc(firestore, 'person', email);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return docSnap.data();
+    } else {
+      console.log('Can not find user');
+    }
+  } catch (error) {
+    console.log('Error fetching user data', error);
   }
 }
